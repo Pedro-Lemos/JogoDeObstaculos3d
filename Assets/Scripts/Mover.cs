141a6287927e3ce37cecfe25ae918836c248370a
Pedro H. Lemos <87588543+Pedro-Lemos@users.noreply.github.com>
2022-04-08 20:18:58 -0300

    Add files via upload

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{   
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game.");
+        Debug.Log("To move around, use the arrow keys.");
+        Debug.Log("Don't get caught by the objects.");
+    }
+    
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);            
+    }
+
+}
---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float xAngle = 0f;
+    [SerializeField] float yAngle = 0.5f;    
+    [SerializeField] float zAngle = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xAngle, yAngle, zAngle);
+    }
+}
